589746486229950e3ad2cbbbb23f7e4c3c8cdcfb
Farooq Ajibade <timiajibade24@gmail.com>
2023-01-28 14:34:01 +0100

    Added memory update route/controller

--- Backend/src/controllers/memoryController.ts
+++ Backend/src/controllers/memoryController.ts
@@ -2,32 +2,65 @@ import express, { Application, Request, Response, NextFunction } from "express";
 import Memory from "../models/memory";
 
 interface Post {
-  user_name: string;
-  photo: string;
-  title: string;
-  description: string;
-  location: string;
-  likes: number;
+  user_name?: string;
+  photo?: string;
+  title?: string;
+  description?: string;
+  location?: string;
+  likes?: number;
   date_created?: string;
   date_updated?: string;
-  comments: [{ user: string; comment: string }];
+  comments?: [{ user: string; comment: string }];
 }
 
-export const createMemory = (req: Request, res: Response) => {
-  const newMemory = new Memory({
-    user_name: "matosky",
-    photo: "img.jpg",
-    title: "skydiving",
-    description: "moving withjam boys",
-    location: "Abuja",
-  });
+export const createMemory = async (req: Request, res: Response) => {
+  try {
+    const newMemory = new Memory({ ...req.body });
+    await newMemory.save();
 
-  newMemory
-    .save()
-    .then((result: any) => {
-      res.send(result);
-    })
-    .catch((err: any) => {
-      console.log(err);
+    res.status(201).json({
+      status: "success",
+      data: {
+        memory: newMemory,
+      },
     });
-}
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
+
+export const updateMemory = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const allowedUpdates = ["description", "title", "likes"];
+  const updates = Object.keys(req.body);
+  const isValidUpdates = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidUpdates)
+    return res.status(400).send({ Error: "Unknown update!" });
+
+  try {
+    const memory: any = await Memory.findOne({ _id: req.params.id });
+    updates.forEach((update) => (memory[update] = req.body[update]));
+    await memory.save();
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        memory,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
