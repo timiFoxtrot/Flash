a499fdb8dcca61a382b3e16e9b356e7aba2cb592
Farooq Ajibade <timiajibade24@gmail.com>
2023-01-31 09:24:24 +0100

    Added new routes and removed node_modules from git

--- Backend/src/routes/memoryRoutes.ts
+++ Backend/src/routes/memoryRoutes.ts
@@ -1,14 +1,23 @@
 import express from "express";
 import { Request, Response } from "express";
-import { createMemory, getOwnMemory, updateMemory } from "../controllers/memoryController";
+import {
+  createMemory,
+  deleteMemory,
+  getAllMemories,
+  getMemoryByUser,
+  getOwnMemory,
+  updateMemory,
+} from "../controllers/memoryController";
 import { getAllUsers } from "../controllers/userController";
 import { auth } from "../middlewares/auth";
 import { Upload } from "../middlewares/imageUpload";
 const router = express.Router();
 
 router.post("/memories", Upload.single("image"), createMemory);
-router.patch("/memories/:id", auth, updateMemory)
-router.get("/memories/:id", auth, getOwnMemory)
-router.get('/memories', auth, getAllUsers)
+router.patch("/memories/:id", auth, updateMemory);
+router.get("/memories/:id", auth, getOwnMemory);
+router.get("/memories", getAllMemories);
+router.get("/memories", auth, getMemoryByUser);
+router.delete("/memories/:id", deleteMemory);
 
 export default router;
